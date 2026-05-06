6e6a8907431cc1d524b157525fd137ccdf42e069
Funmi77 <vrcreativeagency@gmail.com>
2026-05-06 17:56:50 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Funmilayo Fagbemi and is {""}
-        <a
-          href="https://github.com/Funmi77/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Funmilayo Fagbemi and is {""}
+          <a
+            href="https://github.com/Funmi77/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
