7c75a36aa94136a5c5675c48226c77ac8290efed
Funmi77 <vrcreativeagency@gmail.com>
2026-05-06 17:37:20 +0100

    Added Github Repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Funmilayo Fagbemi and is {""}
+        <a
+          href="https://github.com/Funmi77/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
